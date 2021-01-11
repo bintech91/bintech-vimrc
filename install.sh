300f2e068142d28296a7a047b05dd4d96df572af
Tee Pham <phamtan@amazon.com>
2021-01-10 19:05:59 -0800

    install the_silver_searcher and ripgrep for FZF

--- install.sh
+++ install.sh
@@ -19,11 +19,22 @@ if [ -z "$JAVA_HOME" ]; then
         echo "WARNING: JAVA_HOME is empty. Setup default JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64 in .profile to use java plugins"
         echo "export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64" >> ~/.profile
         source ~/.profile
+
+        echo "Install the_silver_searcher"
+        sudo apt-get install silversearcher-ag
+
+        echo "Install ripgrep"
+        sudo apt-get install ripgrep
     elif [[ "$OSTYPE" == "darwin"* ]]; then
         # Mac OSX
         echo "WARNING: JAVA_HOME is empty. Setup default JAVA_HOME=`/usr/libexec/java_home` in .bash_profile to use java plugins"
         echo "export JAVA_HOME=`/usr/libexec/java_home`" >> ~/.bash_profile
         source ~/.bash_profile
+
+        echo "Install the_silver_searcher"
+        brew install the_silver_searcher
+        echo "Install ripgrep"
+        brew install ripgrep
     else
         # Unknown
         echo "WARNING: JAVA_HOME is empty. Please setup your JAVA_HOME to use java plugins"
