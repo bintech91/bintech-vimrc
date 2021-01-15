febe67e437744cfd76ccd3c3cdc9f38f5d5f151b
bintech91 <phamtan15991@gmail.com>
2021-01-15 15:51:13 -0800

    update Ag Rg installation for FZF

--- post_install.sh
+++ post_install.sh
@@ -3,3 +3,9 @@ sed "s|\$HOME|$HOME|g" settings/coc-settings.json > ~/.config/nvim/coc-settings.
 # jdt-language-server-0.57.0-202006300904
 rm -Rf ~/.config/coc/extensions/coc-java-data/server/*
 cp -Rf softwares/jdt-server/* ~/.config/coc/extensions/coc-java-data/server/
+
+# install Ag search for FZF
+brew install the_silver_searcher
+
+# install Rg for FZF
+brew install ripgrep
